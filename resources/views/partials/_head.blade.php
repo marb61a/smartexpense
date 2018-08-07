fee15c1a1fe8d4622108e758e155628d9ce93b59
marb61a <marb61a@gmail.com>
2018-08-07 21:05:32 +0100

    added initial navbar and updated head partial

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,4 +1,13 @@
 <head>
 	<meta charset="UTF-8">
-	<title>Smart Expense</title>
+	<meta name="description" content="Smart Expense Keeping Web Application in Laravel 5.6.*">
+	<meta name="author" content="Martin">
+	
+	<link rel="shortcut icon" href="/img/favicon.png">
+	<link rel="icon" href="/img/favicon.png" type="image/x-icon">
+
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<link rel="stylesheet" href="/css/style.css">
+
+	<title>Smart Expenses</title>
 </head>
---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default navbar-static-top head-style">
+	<div class="container">
+		<div class="row">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand heading-style"> 
+					Onliner
+				</a>
+			</div>
+
+		</div>
+	</div>
+</nav>
