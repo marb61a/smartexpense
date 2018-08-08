fea3e9593d4b2c83a8cff623b55ceea8de2362b1
marb61a <marb61a@gmail.com>
2018-08-08 21:37:52 +0100

    updating reset auth view files

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -15,6 +15,58 @@
                     <h3 class="text-center">Reset Password?</h3>                    
                 </div>
             </div>
+
+            <div class="form-group">
+                <label for="email" class="col-sm-4 form-control-label">
+                    Email Address
+                </label>
+                <div class="col-sm-6">
+                    <input id="email" type="email"  class="form-control" name="email" value="{{ $email ?? old('email') }}" required autofocus>
+
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>
+                                {{ $errors->first('email') }}
+                            </strong>    
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="password" class="col-sm-4 form-control-label">
+                    Password
+                </label>
+                <div class="col-sm-6">
+                    <input id="password" type="password" class="form-control" name="password" required>
+
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>
+                                {{ $errors->first('password') }}
+                            </strong>    
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="password-confirm" class="col-sm-4 form-control-label">
+                    Confirm Password
+                </label>
+                <div class="col-sm-6">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-12">
+                    <button type="submit" class="btn btn-danger btn-block">
+                        Reset Password
+                    </button>
+                </div>
+            </div>
+
         </form>
     </div>
 
