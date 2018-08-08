e42ad56634befc0e0fc266b4d03131c3495f6f43
marb61a <marb61a@gmail.com>
2018-08-08 12:04:15 +0100

    added footer and intial authApp layout file

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class="col-md-12">
+		<div class="col-sm-10 col-offset-1">
+			<div class="nav navbar-inverse footer">
+				<P style="color:white;">
+					<span>Developed By:</span> Martin | 
+					<span>Website:</span> www.martinbyrne.gq | 
+					<span>Email:</span>marb61a@gmail.com 
+				</P>
+			</div>
+		</div>	
+	</div>
+</div>
--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -14,44 +14,44 @@
 					<li><a class="heading-style" href="">Expense Request</a></li>
 					<li><a class="heading-style" href="">Users &nbsp; <i class="fa fa-user"></i></a></li>
 					<li><a class="heading-style" href="">Reports &nbsp; <i class="fa fa-eye"></i></a></li>
+
+					<li>
+						<form class="navbar-form navbar-left" action="#" method="#" role="search">
+							<div class="form-group">
+								<input type="text" class="form-control" name="search" placeholder="Search">
+								<button type="submit" style=" padding: 6px 10px;" class="btn btn-default">
+									<i class="fa fa-search"></i>
+								</button>
+							</div>
+						</form>
+					</li>
+
+					<li>
+						<img 
+							src="/img/favicon.png" 
+							class="img-rounded avatar" 
+							height="40px" 
+							style="position: relative;;margin-top:0px; margin-right: 5px; width: 32px;"
+						>
+					</li>
+
+					<li class="dropdown">
+						<a 
+							href="#" 
+							class="dropdown-toggle dropdown dropdown-border" 
+							data-toggle="dropdown" 
+							role="button"
+						>
+							<i class="fa fa-cog fa-lg heading-style"></i><span class="heading-style caret"></span>	
+						</a>
+
+						<ul class="dropdown-menu">
+							<li><a href="">Name</a></li>
+							<li><a href="">Profile</a></li>
+							<li><a href="{{ route('logout') }}">Logout</a></li>
+						</ul>
+					</li>
 				</ul>
-
-				<li>
-					<form class="navbar-form navbar-left" action="#" method="#" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" name="search" placeholder="Search">
-							<button type="submit" style=" padding: 6px 10px;" class="btn btn-default">
-								<i class="fa fa-search"></i>
-							</button>
-						</div>
-					</form>
-				</li>
-
-				<li>
-					<img 
-						src="/img/favicon.png" 
-						class="img-rounded avatar" 
-						height="40px" 
-						style="position: relative;;margin-top:0px; margin-right: 5px; width: 32px;"
-					>
-				</li>
-
-				<li class="dropdown">
-					<a 
-						href="#" 
-						class="dropdown-toggle dropdown dropdown-border" 
-						data-toggle="dropdown" 
-						role="button"
-					>
-						<i class="fa fa-cog fa-lg heading-style"></i><span class="heading-style caret"></span>	
-					</a>
-
-					<ul class="dropdown-menu">
-						<li><a href="">Name</a></li>
-						<li><a href="">Profile</a></li>
-						<li><a href="{{ route('logout') }}">Logout</a></li>
-					</ul>
-				</li>
 			</div>
 		</div>
 	</div>
