6c1b00f5ae0449c31de0ad5b98c5159074160879
marb61a <marb61a@gmail.com>
2018-08-14 17:22:06 +0100

    added controllers

---
+++ app/Http/Controllers/CompaniesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompaniesController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+    	
+    }
+
+    public function active(){
+    	
+    }
+}
